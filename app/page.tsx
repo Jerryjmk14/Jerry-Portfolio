d3bfc1ce51a28a6b1506b72e687c143a8edfbcee
Jeremiah Egwa <jerriee232@gmail.com>
2024-08-12 04:20:27 +0100

    completed the navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import Image from "next/image";
+import Hero from "../components/Hero.jsx";
 
 export default function Home() {
-  return (
-    <main className="bg-[#0A192F] flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-white">My Portfolia</h1>
-    </main>
-  );
+  return <main className="text-white">{/* <Hero /> */}</main>;
 }
